e9b2ed79217f6bc31f016b935740d5cd6769a0c0
Sanghun <ph38230119@gmail.com>
2025-06-16 23:48:59 +0900

    :speech_balloon: edit description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,20 @@ import Carousel from "./Carousel.jsx";
 export default function Hero() {
   return (
     <section className="mx-auto grid flex-grow grid-cols-1 gap-10 px-8 py-16 md:grid-cols-2 md:items-start md:gap-20 md:px-10 md:py-14">
-      <div className="order-1 mt-0 md:order-1 md:mt-12">
-        <h1 className="text-4xl leading-tight font-extrabold md:text-5xl">
-          Navigate Campus <br className="hidden md:block" /> with Ease
+      <div className="order-1 mt-0 md:order-1 md:mt-24">
+        <h1 className="text-5xl leading-tight font-extrabold md:text-6xl">
+          대전 캠퍼스 셔틀의
+          <br className="hidden md:block" /> 모든 정보를
+          <span className="bg-gradient-to-br from-[#FF79A7] to-[#C873FF] bg-clip-text text-transparent">
+            {" "}
+            한 눈에.
+          </span>
         </h1>
-        <p className="mt-4 max-w-md text-base text-gray-300">
-          Find the best shuttle routes and schedules for your campus. Get
-          real‑time updates and never miss your ride.
+        <p className="mt-8 max-w-md text-lg text-gray-300">
+          대전 캠퍼스 셔틀 버스의 노선과 정류장을 길찾기 앱을 사용하듯 편리하게
+          확인하세요. 정류장을 검색해 정확한 위치를 파악하고, 매 학기
+          업데이트되는 최신 정류장 정보를 활용하세요. App Store, Google Play,
+          혹은 웹에서 바로 만나보세요.
         </p>
 
         {/* 버튼 (데스크탑) */}
