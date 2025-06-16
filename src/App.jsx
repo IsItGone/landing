bf6a110c2d665a5dff3d06a03bc51ee7fbacfe23
Sanghun <ph38230119@gmail.com>
2025-06-16 20:32:44 +0900

    :sparkles: add background gradient

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header.jsx";
 
 export default function App() {
   return (
-    <main className="flex size-full min-h-screen flex-col items-center bg-gradient-to-b from-[#303A52] to-[#242c41] text-white">
+    <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#1B1E37] via-[#3A3F7A] to-[#1B1E37] text-white">
       <div className="max-w-7xl">
         <Header />
         <Hero />
