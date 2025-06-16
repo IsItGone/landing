6f0924b81c590393e1ce1bf5704cbb3f97257921
Sanghun <ph38230119@gmail.com>
2025-06-16 23:49:18 +0900

    :lipstick: edit max width

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/Header.jsx";
 export default function App() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#1B1E37] via-[#3A3F7A] to-[#1B1E37] text-white">
-      <div className="max-w-7xl">
+      <div className="w-full max-w-7xl">
         <Header />
         <Hero />
         <Footer />
