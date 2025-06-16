836f4191adb8879ff14f6420041a8ffbd9f6e063
seojeong <sjytis14@naver.com>
2025-06-16 16:51:32 +0900

    hero 수정 다운로드 버튼 배치 수정

--- src/components/DownloadButtons.jsx
+++ src/components/DownloadButtons.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { QRCodeSVG } from "qrcode.react";
 
 export default function DownloadButtons() {
